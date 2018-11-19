20b9e477b40443cfd91bf868dae0682836992651
Luis Oliveira <luisoliveira98@ua.pt>
2018-11-19 22:53:34 +0000

    Detalhes viagem

--- app/src/main/java/com/example/android/carpoolapp/DetalhesViagem.java
+++ app/src/main/java/com/example/android/carpoolapp/DetalhesViagem.java
@@ -1,7 +1,10 @@
 package com.example.android.carpoolapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DetalhesViagem extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class DetalhesViagem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_viagem);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detalhe_viagem, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()) {
+            case R.id.action_mostrarMapa:
+                //Mostrar mapa
+            case R.id.action_editar:
+                //Editar Viagem
+            case R.id.action_eliminar:
+                //Eliminar Viagem
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
