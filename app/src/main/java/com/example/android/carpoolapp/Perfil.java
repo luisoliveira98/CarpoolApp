fc4a49342774657cd1b0175ec2aeba1ac1659c66
Luis Oliveira <luisoliveira98@ua.pt>
2018-11-19 00:20:54 +0000

    Atualizações na gestão do perfil

--- app/src/main/java/com/example/android/carpoolapp/Perfil.java
+++ app/src/main/java/com/example/android/carpoolapp/Perfil.java
@@ -1,10 +1,14 @@
 package com.example.android.carpoolapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.Menu;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,8 +28,12 @@ public class Perfil extends AppCompatActivity {
                     textNumero,
                     textCidade,
                     textMarcaCarro,
-                    textModeloAno,
-                    textMatricula;
+                    textModelo,
+                    textMatricula,
+                    textCor,
+                    textAno;
+
+    private ImageView userImage;
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
@@ -37,13 +45,16 @@ public class Perfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        userImage = findViewById(R.id.imageUser);
         textNome = findViewById(R.id.textNome);
         textEmail = findViewById(R.id.textEmail);
         textNumero = findViewById(R.id.textNumero);
         textCidade = findViewById(R.id.textCidade);
         textMarcaCarro = findViewById(R.id.textMarcaCarro);
-        textModeloAno = findViewById(R.id.textModeloCarro);
+        textModelo = findViewById(R.id.textModeloCarro);
         textMatricula = findViewById(R.id.textMatriculaCarro);
+        textCor = findViewById(R.id.textCorCarro);
+        textAno = findViewById(R.id.textAnoCarro);
 
         mFirebaseDatabase = FirebaseDatabase.getInstance().getReference();
         mFirebaseAuth = FirebaseAuth.getInstance();
@@ -51,12 +62,42 @@ public class Perfil extends AppCompatActivity {
 
         user = (User) getIntent().getExtras().getSerializable("user");
 
+
         textNome.setText(user.getNome());
+        textEmail.setText(user.getEmail());
+        textCidade.setText(user.getCidade());
+        textNumero.setText(user.getTelemovel());
+        //Carro informações
+        textMarcaCarro.setText(user.getCarro().getMarca());
+        textModelo.setText(user.getCarro().getModelo());
+        textMatricula.setText(user.getCarro().getMatricula());
+        textCor.setText(user.getCarro().getCor());
+        textAno.setText(user.getCarro().getAno());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_perfil, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()) {
+            case R.id.action_updatePerfil:
+                Intent intent = new Intent(this, UpdatePerfil.class);
+                intent.putExtra("user", user);
+                startActivity(intent);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/android/carpoolapp/UpdatePerfil.java
+++ app/src/main/java/com/example/android/carpoolapp/UpdatePerfil.java
@@ -2,12 +2,81 @@ package com.example.android.carpoolapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UpdatePerfil extends AppCompatActivity {
+    User user;
+    EditText editNome,
+            editTelemovel,
+            editCidade,
+            editMarcaCarro,
+            editModeloCarro,
+            editAnoCarro,
+            editCorCarro,
+            editMatriculaCarro;
+
+    DatabaseReference firebaseDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_perfil);
+
+        user = (User) getIntent().getExtras().getSerializable("user");
+
+        firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+
+        editNome = findViewById(R.id.editNomePerfil);
+        editCidade = findViewById(R.id.editCidadePerfil);
+        editTelemovel = findViewById(R.id.editTelemovelPerfil);
+        editMarcaCarro = findViewById(R.id.editMarcaCarro);
+        editModeloCarro = findViewById(R.id.editModeloCarro);
+        editMatriculaCarro = findViewById(R.id.editMatriculaCarro);
+        editAnoCarro = findViewById(R.id.editAnoCarro);
+        editCorCarro = findViewById(R.id.editCorCarro);
+
+        editNome.setText(user.getNome());
+
+        if (!user.getCidade().equals(""))
+            editCidade.setText(user.getCidade());
+        if (!user.getTelemovel().equals(""))
+            editTelemovel.setText(user.getTelemovel());
+
+        if(user.getCarro()!=null) {
+            editModeloCarro.setText(user.getCarro().getModelo());
+            editMarcaCarro.setText(user.getCarro().getMarca());
+            editMatriculaCarro.setText(user.getCarro().getMatricula());
+            editCorCarro.setText(user.getCarro().getCor());
+            editAnoCarro.setText(user.getCarro().getAno());
+        }
+    }
+
+    public void updatePerfil(View view) {
+        //Dados Pessoais
+        String nomeUser = editNome.getText().toString();
+        String cidade = editCidade.getText().toString();
+        String telemovel = editTelemovel.getText().toString();
+
+        //Dados do carro
+        String marcaCarro = editMarcaCarro.getText().toString();
+        String modeloCarro = editModeloCarro.getText().toString();
+        String matriculaCarro = editMatriculaCarro.getText().toString();
+        String corCarro = editCorCarro.getText().toString();
+        String anoCarro = editAnoCarro.getText().toString();
+
+        Carro carro = new Carro(marcaCarro, modeloCarro, corCarro, matriculaCarro, anoCarro);
+        user.setNome(nomeUser);
+        user.setCidade(cidade);
+        user.setTelemovel(telemovel);
+        user.setCarro(carro);
+
+        firebaseDatabase.child("users").child(user.getEmail().split("@")[0]).setValue(user);
+
+        finish();
     }
 }
