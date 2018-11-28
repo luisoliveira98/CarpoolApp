11736578f1a97b8fd59256833ceeb35c27863e91
Ricardo Castor <ricardojscastor@ua.pt>
2018-11-28 00:10:53 +0000

    edq

--- app/src/main/java/com/example/android/carpoolapp/PathViagem.java
+++ app/src/main/java/com/example/android/carpoolapp/PathViagem.java
@@ -8,16 +8,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.json.JSONObject;
@@ -32,260 +36,71 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class PathViagem extends AppCompatActivity implements OnMapReadyCallback {
+public class PathViagem extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
 
     private GoogleMap mMap;
-    ArrayList<LatLng> points;
+    private MarkerOptions p, d;
+    private Polyline polyline;
     private Viagem viagem;
-    private String partida;
-    private String destino;
-    private Marker p, d;
-    private GetDirectionsData getDirectionsData;
-    private String keyViagem;
+    private String partida, destino;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maplayout);
-        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        supportMapFragment.getMapAsync(this);
-        points = new ArrayList<>();
         viagem = (Viagem) getIntent().getExtras().getSerializable("viagem");
         destino = (String) viagem.getPontoDestino();
         partida = (String) viagem.getPontoPartida();
-        keyViagem = (String) getIntent().getExtras().getSerializable("key");
-        //System.out.println("AQUIIIIIIIIIIII:" + keyViagem);
-
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
-    private String getUrl(LatLng origin, LatLng dest) {
-
-        // Origin of route
-        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
-
-        // Destination of route
-        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
-
-
-        // Sensor enabled
-        String sensor = "sensor=false";
-
-        // Building the parameters to the web service
-        String parameters = str_origin + "&" + str_dest + "&" + sensor;
-
-        // Output format
-        String output = "json";
-
-        // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
-
-
-        return url;
-    }
-
-
-    // Fetches data from url passed
-    private class FetchUrl extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... url) {
-
-            // For storing data from web service
-            String data = "";
-
-            try {
-                // Fetching the data from web service
-                data = downloadUrl(url[0]);
-                Log.d("Background Task data", data.toString());
-            } catch (Exception e) {
-                Log.d("Background Task", e.toString());
-            }
-            return data;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            ParserTask parserTask = new ParserTask();
-
-            // Invokes the thread for parsing the JSON data
-            parserTask.execute(result);
-
-        }
-    }
-
-
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL(strUrl);
-
-            // Creating an http connection to communicate with url
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            // Connecting to url
-            urlConnection.connect();
-
-            // Reading data from url
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuffer sb = new StringBuffer();
-
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-            data = sb.toString();
-            Log.d("downloadUrl", data.toString());
-            br.close();
-
-        } catch (Exception e) {
-            Log.d("Exception", e.toString());
-        } finally {
-            iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         Geocoder geocoder = new Geocoder(this);
-        FetchUrl fetchUrl;
-        StringBuilder sb;
-        Object[] dataTransfer = new Object[4];
-
-        // Add a marker in Sydney and move the camera
         try {
-            List<Address> parts = geocoder.getFromLocationName(this.destino, 1);
-            List<Address> dests = geocoder.getFromLocationName(this.partida, 1);
+            List<Address> parts = geocoder.getFromLocationName(this.partida, 1);
+            List<Address> dests = geocoder.getFromLocationName(this.destino, 1);
             Address paddress = parts.get(0);
             Address daddress = dests.get(0);
             String plocality = paddress.getLocality();
             String dlocality = daddress.getLocality();
-            MarkerOptions options = new MarkerOptions();
-            MarkerOptions options1 = new MarkerOptions();
-            options.title(plocality);
-            options1.title(dlocality);
-            options.snippet("Partida");
-            options1.snippet("Destino");
-            options.draggable(false);
-            options1.draggable(false);
-            options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
-            options1.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
-            options.position(new LatLng(paddress.getLatitude(), paddress.getLongitude()));
-            options1.position(new LatLng(daddress.getLatitude(), daddress.getLongitude()));
-            p = mMap.addMarker(options);
-            d = mMap.addMarker(options1);
-            sb = new StringBuilder();
-            sb.append("https://maps.googleapis.com/maps/api/directions/json?");
-            sb.append("origin=" + paddress.getLatitude() + "," + paddress.getLongitude());
-            sb.append("&destination=" + daddress.getLatitude() + "," + daddress.getLongitude());
-            sb.append("&key=" + "KEY HERE");
-            getDirectionsData = new GetDirectionsData(getApplicationContext());
-            dataTransfer[0]=mMap;
-            dataTransfer[1]=sb.toString();
-            dataTransfer[2]=new LatLng(paddress.getLatitude(), paddress.getLongitude());
-            dataTransfer[3]=new LatLng(daddress.getLatitude(), daddress.getLongitude());
-            String url = getUrl(new LatLng(paddress.getLatitude(), paddress.getLongitude()), new LatLng(daddress.getLatitude(), daddress.getLongitude()));
-            fetchUrl = new FetchUrl();
-            fetchUrl.execute(url);
+            p = new MarkerOptions().position(new LatLng(paddress.getLatitude(), paddress.getLongitude())).title(plocality).snippet("Partida").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+            d = new MarkerOptions().position(new LatLng(daddress.getLatitude(), daddress.getLongitude())).title(dlocality).snippet("Destino").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
             LatLng latLng = new LatLng(paddress.getLatitude(), paddress.getLongitude());
             CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, 8);
+            mMap.addMarker(p);
+            mMap.addMarker(d);
             mMap.moveCamera(update);
-            getDirectionsData.execute(dataTransfer);
+            new FetchURL(PathViagem.this).execute(getUrl(p.getPosition(), d.getPosition(), "driving"), "driving");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
     }
 
-
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
-
-        // Parsing the data in non-ui thread
-        @Override
-        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-
-            JSONObject jObject;
-            List<List<HashMap<String, String>>> routes = null;
-
-            try {
-                jObject = new JSONObject(jsonData[0]);
-                Log.d("ParserTask",jsonData[0].toString());
-                DataParser parser = new DataParser();
-                Log.d("ParserTask", parser.toString());
-
-                // Starts parsing data
-                routes = parser.parse(jObject);
-                Log.d("ParserTask","Executing routes");
-                Log.d("ParserTask",routes.toString());
-
-            } catch (Exception e) {
-                Log.d("ParserTask",e.toString());
-                e.printStackTrace();
-            }
-            return routes;
-        }
-
-        // Executes in UI thread, after the parsing process
-        @Override
-        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-            ArrayList<LatLng> points;
-            PolylineOptions lineOptions = null;
-
-            // Traversing through all the routes
-            for (int i = 0; i < result.size(); i++) {
-                points = new ArrayList<>();
-                lineOptions = new PolylineOptions();
-
-                // Fetching i-th route
-                List<HashMap<String, String>> path = result.get(i);
-
-                // Fetching all the points in i-th route
-                for (int j = 0; j < path.size(); j++) {
-                    HashMap<String, String> point = path.get(j);
-
-                    double lat = Double.parseDouble(point.get("lat"));
-                    double lng = Double.parseDouble(point.get("lng"));
-                    LatLng position = new LatLng(lat, lng);
-
-                    points.add(position);
-                }
-
-                // Adding all the points in the route to LineOptions
-                lineOptions.addAll(points);
-                lineOptions.width(10);
-                lineOptions.color(Color.RED);
-
-                Log.d("onPostExecute","onPostExecute lineoptions decoded");
-
-            }
-
-            // Drawing polyline in the Google Map for the i-th route
-            if(lineOptions != null) {
-                mMap.addPolyline(lineOptions);
-            }
-            else {
-                Log.d("onPostExecute","without Polylines drawn");
-            }
-        }
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Mode
+        String mode = "mode=" + directionMode;
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=AIzaSyBhAAum8BaAIlIIuYrXSmPyVkQNROxbd2M";
+        return url;
     }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (polyline != null)
+            polyline.remove();
+        polyline = mMap.addPolyline((PolylineOptions) values[0]);
+    }
+
 }
---
+++ app/src/main/java/com/example/android/carpoolapp/TaskLoadedCallback.java
@@ -0,0 +1,5 @@
+package com.example.android.carpoolapp;
+
+public interface TaskLoadedCallback {
+    void onTaskDone(Object... values);
+}
